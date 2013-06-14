fa3f2016551f39748ca0ce5876f5dfc4e933c91e
Francisco Souza <f@souza.cc>
2013-06-14 18:00:51 -0300

    testing/server: draft the public API

--- testing/server.go
+++ testing/server.go
@@ -8,8 +8,38 @@ package testing
 
 import (
 	"github.com/dotcloud/docker"
+	"net"
+	"net/http"
 )
 
+// DockerServer represents a programmable, concurrent, HTTP server implementing
+// a fake version of the Docker remote API.
+//
+// It can used in standalone mode, listening for connections or as an arbitrary
+// HTTP handler.
+//
+// For more details on the remote API, check http://goo.gl/yMI1S.
 type DockerServer struct {
 	containers []docker.Container
+	images     []docker.Image
+	listener   net.Listener
+}
+
+// NewServer returns a new instance of the fake server, in standalone mode. Use
+// the method URL to get the URL of the server.
+func NewServer() (*DockerServer, error) {
+	return nil, nil
+}
+
+// Stop stops the server.
+func (s *DockerServer) Stop() {
+}
+
+// URL returns the HTTP URL of the server.
+func (s *DockerServer) URL() string {
+	return ""
+}
+
+// ServeHTTP handles HTTP requests sent to the server.
+func (s *DockerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
