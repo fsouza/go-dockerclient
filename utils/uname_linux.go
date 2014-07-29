c2f92029af3685b6ae668ec5749f9a0c66378ef7
Karan Misra <kidoman@gmail.com>
2014-07-30 01:42:51 +0530

    utils: appengine compatibility

--- utils/uname_linux.go
+++ utils/uname_linux.go
@@ -1,3 +1,5 @@
+// +build !appengine
+
 // Copyright 2014 Docker authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the DOCKER-LICENSE file.
