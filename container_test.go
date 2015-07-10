522a2aeef0956609bf7261a743fe66eb5d6ebd16
bfosberry <brendan@codeship.com>
2015-07-10 13:54:08 +0200

    Updating volumes-from to be []string, not string

--- container_test.go
+++ container_test.go
@@ -182,7 +182,7 @@ func TestInspectContainer(t *testing.T) {
                      ],
                      "Image": "base",
                      "Volumes": {},
-                     "VolumesFrom": "",
+                     "VolumesFrom": [],
                      "SecurityOpt": [
                          "label:user:USER"
                       ],
@@ -503,7 +503,7 @@ func TestInspectContainerNegativeSwap(t *testing.T) {
                      ],
                      "Image": "base",
                      "Volumes": {},
-                     "VolumesFrom": ""
+                     "VolumesFrom": []
              },
              "State": {
                      "Running": false,
