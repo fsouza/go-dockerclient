2b5969e13e3e551148b2d87018b3a521fa069276
bfosberry <brendan@codeship.com>
2015-08-10 10:40:49 -0500

    Adding timeout to log stream

--- container.go
+++ container.go
@@ -1100,6 +1100,7 @@ func (c *Client) Logs(opts LogsOptions) error {
 		setRawTerminal: opts.RawTerminal,
 		stdout:         opts.OutputStream,
 		stderr:         opts.ErrorStream,
+		timeout:        30 * time.Minute,
 	})
 }
 
