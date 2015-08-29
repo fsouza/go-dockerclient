93f4e21901137cc566b9ba63236bb0c2f233ed8a
Peter Edge <peter.edge@gmail.com>
2015-08-29 16:39:15 -0500

    tests for RemoveVolume function

--- volume_test.go
+++ volume_test.go
@@ -7,6 +7,7 @@ package docker
 import (
 	"encoding/json"
 	"net/http"
+	"net/url"
 	"reflect"
 	"testing"
 )
@@ -37,5 +38,40 @@ func TestListVolumes(t *testing.T) {
 	}
 	if !reflect.DeepEqual(images, expected) {
 		t.Errorf("ListVolumes: Wrong return value. Want %#v. Got %#v.", expected, images)
+	}
+}
+
+func TestRemoveVolume(t *testing.T) {
+	name := "test"
+	fakeRT := &FakeRoundTripper{message: "", status: http.StatusNoContent}
+	client := newTestClient(fakeRT)
+	err := client.RemoveVolume(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req := fakeRT.requests[0]
+	expectedMethod := "DELETE"
+	if req.Method != expectedMethod {
+		t.Errorf("RemoveVolume(%q): Wrong HTTP method. Want %s. Got %s.", name, expectedMethod, req.Method)
+	}
+	u, _ := url.Parse(client.getURL("/volumes/" + name))
+	if req.URL.Path != u.Path {
+		t.Errorf("RemoveVolume(%q): Wrong request path. Want %q. Got %q.", name, u.Path, req.URL.Path)
+	}
+}
+
+func TestRemoveVolumeNotFound(t *testing.T) {
+	client := newTestClient(&FakeRoundTripper{message: "no such volume", status: http.StatusNotFound})
+	err := client.RemoveVolume("test:")
+	if err != ErrNoSuchVolume {
+		t.Errorf("RemoveVolume: wrong error. Want %#v. Got %#v.", ErrNoSuchVolume, err)
+	}
+}
+
+func TestRemoveVolumeInUse(t *testing.T) {
+	client := newTestClient(&FakeRoundTripper{message: "volume in use and cannot be removed", status: http.StatusConflict})
+	err := client.RemoveVolume("test:")
+	if err != ErrVolumeInUse {
+		t.Errorf("RemoveVolume: wrong error. Want %#v. Got %#v.", ErrVolumeInUse, err)
 	}
 }
