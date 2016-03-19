a0c64ecd06985de396300db328ede7e30b188c09
Francisco Souza <f@souza.cc>
2016-03-19 09:40:13 -0300

    travis: add a script for running tests
    This way we can skip golint on Go 1.3 and 1.4. I'm probably just going
    to revert this commit as soon as we get Go 1.7.

---
+++ travis-scripts/run-tests.bash
@@ -0,0 +1,11 @@
+#!/bin/bash -el
+
+# Copyright 2016 go-dockerclient authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+if ! [[ $TRAVIS_GO_VERSION =~ ^1\.[34] ]]; then
+	make lint
+fi
+
+make vet fmtcheck gotest
