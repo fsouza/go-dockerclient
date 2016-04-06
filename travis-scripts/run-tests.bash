bf97c77db7c945cbcdbf09d56c6f87a66f54537b
Francisco Souza <f@souza.cc>
2016-04-06 19:56:49 -0300

    travis: do not run vet on go 1.4

--- travis-scripts/run-tests.bash
+++ travis-scripts/run-tests.bash
@@ -5,10 +5,10 @@
 # license that can be found in the LICENSE file.
 
 if ! [[ $TRAVIS_GO_VERSION =~ ^1\.[34] ]]; then
-	make lint
+	make lint vet
 fi
 
-make vet fmtcheck gotest
+make fmtcheck gotest
 
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
 	DOCKER_HOST=tcp://127.0.0.1:2375 make integration
