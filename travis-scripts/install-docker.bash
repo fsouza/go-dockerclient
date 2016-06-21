3c3341e082f778f28a186b9d4b97e825f41f0d5d
Francisco Souza <f@souza.cc>
2016-06-21 20:59:34 -0300

    travis: add Docker 1.12.0-rc2 to test list

--- travis-scripts/install-docker.bash
+++ travis-scripts/install-docker.bash
@@ -12,6 +12,7 @@ if [[ $TRAVIS_OS_NAME == "linux" ]]; then
 	set -e
 	sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 	echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+	echo "deb https://apt.dockerproject.org/repo ubuntu-trusty testing" | sudo tee -a /etc/apt/sources.list.d/docker.list
 	sudo apt-get update
 	sudo apt-get install docker-engine=${DOCKER_VERSION}-0~$(lsb_release -cs) -y --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 fi
