4a2e5288244cd60febbfc7bdd07891eab9efa6c1
Francisco Souza <f@souza.cc>
2016-08-12 11:45:23 -0400

    travis-script: start docker after install
    Should fix the build with Docker 1.12.0.

--- travis-scripts/install-docker.bash
+++ travis-scripts/install-docker.bash
@@ -15,4 +15,5 @@ if [[ $TRAVIS_OS_NAME == "linux" ]]; then
 	echo "deb https://apt.dockerproject.org/repo ubuntu-trusty testing" | sudo tee -a /etc/apt/sources.list.d/docker.list
 	sudo apt-get update
 	sudo apt-get install docker-engine=${DOCKER_VERSION}-0~$(lsb_release -cs) -y --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+	sudo start docker || true
 fi
