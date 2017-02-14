873df7501fbdcf9156f782771469d1476f735e2d
Francisco Souza <f@souza.cc>
2017-02-13 19:52:31 -0500

    client_windows_test: add space after buildtag

--- client_windows_test.go
+++ client_windows_test.go
@@ -1,4 +1,5 @@
 // +build windows
+
 // Copyright 2016 go-dockerclient authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
