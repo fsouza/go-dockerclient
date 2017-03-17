aaca8d526691e9a859af69948b306aaf981a2b6f
Julian Einwag <julian@einwag.de>
2017-03-17 17:36:52 +0100

    support authentication when updating swarm mode service from private registries

--- service_test.go
+++ service_test.go
@@ -173,6 +173,54 @@ func TestUpdateService(t *testing.T) {
 	}
 }
 
+func TestUpdateServiceWithAuthentication(t *testing.T) {
+	fakeRT := &FakeRoundTripper{message: "", status: http.StatusOK}
+	client := newTestClient(fakeRT)
+	id := "4fa6e0f0c6786287e131c3852c58a2e01cc697a68231826813597e4994f1d6e2"
+	update := UpdateServiceOptions{Version: 23}
+	update.Auth = AuthConfiguration{
+		Username: "gopher",
+		Password: "gopher123",
+		Email:    "gopher@tsuru.io",
+	}
+
+	err := client.UpdateService(id, update)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req := fakeRT.requests[0]
+	if req.Method != "POST" {
+		t.Errorf("UpdateService: wrong HTTP method. Want %q. Got %q.", "POST", req.Method)
+	}
+	expectedURL, _ := url.Parse(client.getURL("/services/" + id + "/update?version=23"))
+	if gotURI := req.URL.RequestURI(); gotURI != expectedURL.RequestURI() {
+		t.Errorf("UpdateService: Wrong path in request. Want %q. Got %q.", expectedURL.RequestURI(), gotURI)
+	}
+	expectedContentType := "application/json"
+	if contentType := req.Header.Get("Content-Type"); contentType != expectedContentType {
+		t.Errorf("UpdateService: Wrong content-type in request. Want %q. Got %q.", expectedContentType, contentType)
+	}
+	var out UpdateServiceOptions
+	if err := json.NewDecoder(req.Body).Decode(&out); err != nil {
+		t.Fatal(err)
+	}
+	var updateAuth AuthConfiguration
+
+	auth, err := base64.URLEncoding.DecodeString(req.Header.Get("X-Registry-Auth"))
+	if err != nil {
+		t.Errorf("UpdateService: caught error decoding auth. %#v", err.Error())
+	}
+
+	err = json.Unmarshal(auth, &updateAuth)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(updateAuth, update.Auth) {
+		t.Errorf("UpdateService: wrong auth configuration. Want %#v. Got %#v", update.Auth, updateAuth)
+	}
+}
+
 func TestUpdateServiceNotFound(t *testing.T) {
 	client := newTestClient(&FakeRoundTripper{message: "no such service", status: http.StatusNotFound})
 	update := UpdateServiceOptions{}
