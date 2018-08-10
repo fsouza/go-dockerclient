556c7a2a9f14417e2c92ae2882e01dde7375902a
George MacRorie <me@georgemac.com>
2018-08-10 17:41:21 +0100

    Revert "Drop username from IdentityToken auth"
    This reverts commit ae31b474ad6467e330e20015e2dcf6f00c2d5b91.

--- auth.go
+++ auth.go
@@ -152,6 +152,7 @@ func authConfigs(confs map[string]dockerConfig) (*AuthConfigurations, error) {
 
 			// docker config contains username and identitytoken
 			c.Configs[reg] = AuthConfiguration{
+				Username:      string(data),
 				IdentityToken: conf.IdentityToken,
 			}
 
