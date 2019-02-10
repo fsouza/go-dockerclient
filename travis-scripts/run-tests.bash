c93eaeebc0c7478d22df72849e81b94afe218436
Francisco Souza <fsouza@users.noreply.github.com>
2019-02-10 12:37:16 -0500

    travis/run-tests: skip gofmt on windows
    Don't want to fight line endings :)

--- travis-scripts/run-tests.bash
+++ travis-scripts/run-tests.bash
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-if [[ $TRAVIS_GO_VERSION == 1.10* ]]; then
+if [[ $TRAVIS_GO_VERSION == 1.10* || $TRAVIS_OS_NAME == "windows" ]]; then
 	export SKIP_FMT_CHECK=1
 fi
 
