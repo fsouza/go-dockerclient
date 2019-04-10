817654491fee0e4779603a69269797357a02e758
Francisco Souza <fsouza@users.noreply.github.com>
2019-04-10 17:34:41 -0400

    client: remove redundant build tags

--- client_windows_test.go
+++ client_windows_test.go
@@ -1,5 +1,3 @@
-// +build windows
-
 // Copyright 2016 go-dockerclient authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
