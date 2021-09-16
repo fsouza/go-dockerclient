0f57349a7248b9b35ad2193ffe70953d5893e2b8
francisco souza <fsouza@users.noreply.github.com>
2021-09-15 22:36:09 -0400

    Reformat code
    Living the Go 1.17 life.

--- client_unix_test.go
+++ client_unix_test.go
@@ -1,4 +1,6 @@
+//go:build !windows
 // +build !windows
+
 // Copyright 2016 go-dockerclient authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
