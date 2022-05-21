cd557537372b33f703193460577a4d226a04ac42
Ruins <ruinscorocoro+github@gmail.com>
2022-05-21 16:42:57 +0900

    chore: add fields definition for DockerStat API of Docker host uses cgroup2
    Docker Engine 20.10.0+ uses cgroup2 instead of cgroup by default when available (cf. https://github.com/moby/moby/pull/40846 ).
    Using cgroup2 varies the output of DockerStats API.
    In particular, there are many new fields on Stats.MemoryStats.Stats.
    Therefore this commit adds the fields for new fields on cgroup2 to `Stats` struct.

--- container_stats_test.go
+++ container_stats_test.go
@@ -9,7 +9,8 @@ import (
 	"testing"
 )
 
-func TestStats(t *testing.T) {
+// The test for Docker Stat API of the host uses cgroup
+func TestStatsV1(t *testing.T) {
 	t.Parallel()
 	jsonStats1 := `{
        "read" : "2015-01-08T22:57:31.547920715Z",
@@ -260,6 +261,290 @@ func TestStats(t *testing.T) {
           "system_cpu_usage" : 20091722000000000,
 		  "online_cpus": 4
        }
+    }`
+	var expected1 Stats
+	var expected2 Stats
+	err := json.Unmarshal([]byte(jsonStats1), &expected1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = json.Unmarshal([]byte(jsonStats2), &expected2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	id := "4fa6e0f0"
+
+	var req http.Request
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(jsonStats1))
+		w.Write([]byte(jsonStats2))
+		req = *r
+	}))
+	defer server.Close()
+	client, _ := NewClient(server.URL)
+	client.SkipServerVersionCheck = true
+	errC := make(chan error, 1)
+	statsC := make(chan *Stats)
+	done := make(chan bool)
+	defer close(done)
+	go func() {
+		errC <- client.Stats(StatsOptions{ID: id, Stats: statsC, Stream: true, Done: done})
+		close(errC)
+	}()
+	var resultStats []*Stats
+	for {
+		stats, ok := <-statsC
+		if !ok {
+			break
+		}
+		resultStats = append(resultStats, stats)
+	}
+	err = <-errC
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(resultStats) != 2 {
+		t.Fatalf("Stats: Expected 2 results. Got %d.", len(resultStats))
+	}
+	if !reflect.DeepEqual(resultStats[0], &expected1) {
+		t.Errorf("Stats: Expected:\n%+v\nGot:\n%+v", expected1, resultStats[0])
+	}
+	if !reflect.DeepEqual(resultStats[1], &expected2) {
+		t.Errorf("Stats: Expected:\n%+v\nGot:\n%+v", expected2, resultStats[1])
+	}
+	if req.Method != http.MethodGet {
+		t.Errorf("Stats: wrong HTTP method. Want GET. Got %s.", req.Method)
+	}
+	u, _ := url.Parse(client.getURL("/containers/" + id + "/stats"))
+	if req.URL.Path != u.Path {
+		t.Errorf("Stats: wrong HTTP path. Want %q. Got %q.", u.Path, req.URL.Path)
+	}
+}
+
+// The test for Docker Stat API of the host uses cgroup2
+func TestStatsV2(t *testing.T) {
+	t.Parallel()
+	jsonStats1 := `{
+      "read": "2022-05-18T14:37:18.175989615Z",
+      "preread": "2022-05-18T14:37:17.171501052Z",
+      "pids_stats": {
+        "current": 29,
+        "limit": 76948
+      },
+      "blkio_stats": {
+        "io_service_bytes_recursive": [
+          {
+            "major": 259,
+            "minor": 0,
+            "op": "read",
+            "value": 11390976
+          },
+          {
+            "major": 259,
+            "minor": 0,
+            "op": "write",
+            "value": 0
+          }
+        ],
+        "io_serviced_recursive": null,
+        "io_queue_recursive": null,
+        "io_service_time_recursive": null,
+        "io_wait_time_recursive": null,
+        "io_merged_recursive": null,
+        "io_time_recursive": null,
+        "sectors_recursive": null
+      },
+      "num_procs": 0,
+      "storage_stats": {},
+      "cpu_stats": {
+        "cpu_usage": {
+          "total_usage": 185266562000,
+          "usage_in_kernelmode": 37912635000,
+          "usage_in_usermode": 147353926000
+        },
+        "system_cpu_usage": 26707255190000000,
+        "online_cpus": 24,
+        "throttling_data": {
+          "periods": 0,
+          "throttled_periods": 0,
+          "throttled_time": 0
+        }
+      },
+      "precpu_stats": {
+        "cpu_usage": {
+          "total_usage": 185266562000,
+          "usage_in_kernelmode": 37912635000,
+          "usage_in_usermode": 147353926000
+        },
+        "system_cpu_usage": 26707231080000000,
+        "online_cpus": 24,
+        "throttling_data": {
+          "periods": 0,
+          "throttled_periods": 0,
+          "throttled_time": 0
+        }
+      },
+      "memory_stats": {
+        "usage": 28557312,
+        "stats": {
+          "active_anon": 4096,
+          "active_file": 7446528,
+          "anon": 16572416,
+          "anon_thp": 0,
+          "file": 10829824,
+          "file_dirty": 0,
+          "file_mapped": 9740288,
+          "file_writeback": 0,
+          "inactive_anon": 8069120,
+          "inactive_file": 11882496,
+          "kernel_stack": 475136,
+          "pgactivate": 241,
+          "pgdeactivate": 253,
+          "pgfault": 7714,
+          "pglazyfree": 3042,
+          "pglazyfreed": 967,
+          "pgmajfault": 155,
+          "pgrefill": 301,
+          "pgscan": 1802,
+          "pgsteal": 1100,
+          "shmem": 0,
+          "slab": 488920,
+          "slab_reclaimable": 159664,
+          "slab_unreclaimable": 329256,
+          "sock": 0,
+          "thp_collapse_alloc": 0,
+          "thp_fault_alloc": 0,
+          "unevictable": 0,
+          "workingset_activate": 0,
+          "workingset_nodereclaim": 0,
+          "workingset_refault": 0
+        },
+        "limit": 67353382912
+      },
+      "networks": {
+        "eth0": {
+          "rx_bytes": 96802652,
+          "rx_packets": 623704,
+          "rx_errors": 0,
+          "rx_dropped": 0,
+          "tx_bytes": 16597749,
+          "tx_packets": 91982,
+          "tx_errors": 0,
+          "tx_dropped": 0
+        }
+      }
+    }`
+	// 1 second later, shmem is 100
+	jsonStats2 := `{
+      "read": "2022-05-18T14:37:18.175989615Z",
+      "preread": "2022-05-18T14:37:17.171501052Z",
+      "pids_stats": {
+        "current": 29,
+        "limit": 76948
+      },
+      "blkio_stats": {
+        "io_service_bytes_recursive": [
+          {
+            "major": 259,
+            "minor": 0,
+            "op": "read",
+            "value": 11390976
+          },
+          {
+            "major": 259,
+            "minor": 0,
+            "op": "write",
+            "value": 0
+          }
+        ],
+        "io_serviced_recursive": null,
+        "io_queue_recursive": null,
+        "io_service_time_recursive": null,
+        "io_wait_time_recursive": null,
+        "io_merged_recursive": null,
+        "io_time_recursive": null,
+        "sectors_recursive": null
+      },
+      "num_procs": 0,
+      "storage_stats": {},
+      "cpu_stats": {
+        "cpu_usage": {
+          "total_usage": 185266562000,
+          "usage_in_kernelmode": 37912635000,
+          "usage_in_usermode": 147353926000
+        },
+        "system_cpu_usage": 26707255190000000,
+        "online_cpus": 24,
+        "throttling_data": {
+          "periods": 0,
+          "throttled_periods": 0,
+          "throttled_time": 0
+        }
+      },
+      "precpu_stats": {
+        "cpu_usage": {
+          "total_usage": 185266562000,
+          "usage_in_kernelmode": 37912635000,
+          "usage_in_usermode": 147353926000
+        },
+        "system_cpu_usage": 26707231080000000,
+        "online_cpus": 24,
+        "throttling_data": {
+          "periods": 0,
+          "throttled_periods": 0,
+          "throttled_time": 0
+        }
+      },
+      "memory_stats": {
+        "usage": 28557312,
+        "stats": {
+          "active_anon": 4096,
+          "active_file": 7446528,
+          "anon": 16572416,
+          "anon_thp": 0,
+          "file": 10829824,
+          "file_dirty": 0,
+          "file_mapped": 9740288,
+          "file_writeback": 0,
+          "inactive_anon": 8069120,
+          "inactive_file": 11882496,
+          "kernel_stack": 475136,
+          "pgactivate": 241,
+          "pgdeactivate": 253,
+          "pgfault": 7714,
+          "pglazyfree": 3042,
+          "pglazyfreed": 967,
+          "pgmajfault": 155,
+          "pgrefill": 301,
+          "pgscan": 1802,
+          "pgsteal": 1100,
+          "shmem": 100,
+          "slab": 488920,
+          "slab_reclaimable": 159664,
+          "slab_unreclaimable": 329256,
+          "sock": 0,
+          "thp_collapse_alloc": 0,
+          "thp_fault_alloc": 0,
+          "unevictable": 0,
+          "workingset_activate": 0,
+          "workingset_nodereclaim": 0,
+          "workingset_refault": 0
+        },
+        "limit": 67353382912
+      },
+      "networks": {
+        "eth0": {
+          "rx_bytes": 96802652,
+          "rx_packets": 623704,
+          "rx_errors": 0,
+          "rx_dropped": 0,
+          "tx_bytes": 16597749,
+          "tx_packets": 91982,
+          "tx_errors": 0,
+          "tx_dropped": 0
+        }
+      }
     }`
 	var expected1 Stats
 	var expected2 Stats
